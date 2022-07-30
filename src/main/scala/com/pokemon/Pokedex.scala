444fd1c16b238be69718a1c26eca19925080d1af
Alpha Keita <78216570+alphakta@users.noreply.github.com>
2022-07-31 00:39:42 +0200

    last modif

--- src/main/scala/com/pokemon/Pokedex.scala
+++ src/main/scala/com/pokemon/Pokedex.scala
@@ -1,10 +1,65 @@
 package com.pokemon
-
-import okhttp3.{OkHttpClient, Request}
-
-import scala.util.control.NonFatal
+import com.pokemon.services.AOEAsyncServiceOnHttp
+import monix.execution.Scheduler.Implicits.global
 
 object Pokedex {
 
-  def main(args: Array[String]): Unit = println("Hello pokédex")
+  def main(args: Array[String]): Unit =
+    val aoeSvc = new AOEAsyncServiceOnHttp(http = new AsyncHttp)
+    /*
+    ----- GET -> Le pokemon avec le nom bulbasaur
+       aoeSvc.getPokemon("bulbasaur")
+      .runAsync {
+        case Left(err) =>
+          println(s"Got err: ${err.toString}")
+        case Right(result) =>
+          println(result)
+      } */
+
+    /* 
+    ----- WHEREIS -> Tous les pokemons de cerulean-city-area
+      aoeSvc.getPokemonLocation("cerulean-city-area")
+      .runAsync {
+        case Left(err) =>
+          println(s"Got err: ${err.toString}")
+        case Right(result) =>
+          println(result)
+      } */
+
+     
+    // // ----- GETALLTYPE -> Tous les pokemons de type ground
+    //   aoeSvc.getPokemonByType("ground")
+    //   .runAsync {
+    //     case Left(err) =>
+    //       println(s"Got err: ${err.toString}")
+    //     case Right(result) =>
+    //       println(result)
+    //   }
+
+    // // ----- GETALLGENERATION -> Tous les pokemons de generation-i
+    //   aoeSvc.getPokemonByGeneration("generation-i")
+    //   .runAsync {
+    //     case Left(err) =>
+    //       println(s"Got err: ${err.toString}")
+    //     case Right(result) =>
+    //       println(result)
+    //   } 
+
+    println("<>--------------------------- POKEDEX API ---------------------------<>")
+    println("PokedexAPI>")
+
+    //Je n'ai pas pu tester les commandes par ligne de commande car j'ai une erreur avec mon Debug Console...
+    val a = scala.io.StdIn.readLine()
+    var args = a.split(" ").toList
+
+    args match {
+      case List("GET", pokemonName) => aoeSvc.getPokemon(args(1)).runAsync { case Left(err) => println(s"Got err: ${err.toString}") case Right(result) => println(result) }
+      case List("GETALLTYPE", pokemonType) => aoeSvc.getPokemonByType(args(1)).runAsync { case Left(err) => println(s"Got err: ${err.toString}") case Right(result) => println(result) }
+      case List("GETALLGENERATION", pokemonType) => aoeSvc.getPokemonByGeneration(args(1)).runAsync { case Left(err) => println(s"Got err: ${err.toString}") case Right(result) => println(result) }
+      case List("WHEREIS", pokemonLocation) => aoeSvc.getPokemonLocation(args(1)).runAsync { case Left(err) => println(s"Got err: ${err.toString}") case Right(result) => println(result) }
+      case List("MATCH", pokemonType) => println("")
+      case List("QUIT") => System.exit(0)
+      case _ => println("Commande inconnue")
+    }
+
 }
--- src/main/scala/com/pokemon/services/AOEService.scala
+++ src/main/scala/com/pokemon/services/AOEService.scala
@@ -1,36 +1,26 @@
 package com.pokemon.services
 
-import com.pokemon.models.{PokemonInfo, PokemonLocation, Civilization, AOEUnit}
 import monix.eval.Task
 import com.pokemon.Http
 import spray.json._
 import DefaultJsonProtocol._
-import com.pokemon.json.JsonFormatters._
 import com.pokemon.AsyncHttp
 
 
 trait AOEAsyncService {
-  def getCivilizations(): Task[Seq[Civilization]]
-  def getPokemon(name: String): Task[Seq[PokemonInfo]]
+  def getPokemon(name: String): Task[String]
   def getPokemonLocation(name: String): Task[String]
+  def getPokemonByType(typePoke: String): Task[String]
+  def getPokemonByGeneration(generation: String): Task[String]
 }
 
 class AOEAsyncServiceOnHttp(http: AsyncHttp) extends AOEAsyncService {
 
-  def getCivilizations(): Task[Seq[Civilization]] =
-    http.get("https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations")
-      .map(content => content
-        .parseJson.asJsObject
-        .fields("civilizations")
-        .convertTo[Seq[Civilization]]
-      )
-
-  def getPokemon(name: String): Task[Seq[PokemonInfo]] =
+  def getPokemon(name: String): Task[String] =
     http.get("https://pokeapi.co/api/v2/pokemon/"+name+"/")
       .map(content => content
         .parseJson
-        .asJsObject
-        .convertTo[Seq[PokemonInfo]]
+        .toString
       )
 
   def getPokemonLocation(name: String): Task[String] = 
@@ -42,4 +32,18 @@ class AOEAsyncServiceOnHttp(http: AsyncHttp) extends AOEAsyncService {
         // .asJsObject
         // .convertTo[Seq[PokemonLocation]]
       )
+
+  def getPokemonByType(typePoke: String): Task[String] =
+      http.get("https://pokeapi.co/api/v2/type/"+typePoke+"/")
+        .map(content => content
+        .parseJson
+        .toString
+        )
+
+  def getPokemonByGeneration(generation: String): Task[String] = 
+      http.get("https://pokeapi.co/api/v2/generation/"+generation+"/")
+        .map(content => content
+        .parseJson
+        .toString
+        )
 }
